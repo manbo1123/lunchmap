996fa7282ff04b82b3d675b92dcc5cbe2cf1da10
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-11-24 13:01:48 +0900

    add_enum-laravel

---
+++ database/migrations/2020_11_24_100741_add_column_status.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\Status;
+
+class AddColumnStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            $table->enum('status', Status::getValues())
+                ->default(Status::UnApplied)
+                ->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
